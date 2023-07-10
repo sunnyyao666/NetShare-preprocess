3d7130d599aee6ac080c90ecb5fd78353a507a0d
Hongtao Yao <hyao2@andrew.cmu.edu>
2023-07-10 19:17:43 -0400

    Initial commit

---
+++ parse2csv.py
@@ -0,0 +1,86 @@
+import json
+import pandas as pd
+import parse_func
+from config_io import Config
+
+
+def get_fields(fields):
+    """
+    Extract field configurations from the origin fields config and fill in default values.
+    :param fields: Origin fields config
+    :return: Extracted fields with filled default values
+    """
+    result = []
+    for data_type in fields:
+        for field in fields[data_type]:
+            field['type'] = data_type
+            # Default for to_name: origin name
+            if 'to' not in field:
+                field['to'] = field['name']
+            # Default for format: str
+            if 'format' not in field:
+                field['format'] = 'str'
+            # Default for abnormal: false
+            if 'abnormal' not in field:
+                field['abnormal'] = False
+            result.append(field)
+    return result
+
+
+def parse_field(fields, df, result):
+    """
+    Parse all the fields one by one according to the config.
+    :param fields: Fields configs
+    :param df: Origin dataframe
+    :param result: Parsed dataframe
+    """
+    for field in fields:
+        result[field['to']] = df[field['name']].apply(getattr(parse_func, 'parse', None), field=field,
+                                                      if_handle_abnormal=field['abnormal'],
+                                                      func_name=field.get('parse', None))
+        result[field['to']] = result[field['to']].astype(field['format'], errors='ignore')
+
+
+def to_dataframe(input_config):
+    """
+    Extract origin input file to pandas dataframe.
+    :param input_config: Input config with file path and format
+    :return: Extracted pandas dataframe
+    """
+    input_format = input_config.get('format', 'zeek_log_json')  # Default: 'zeek_log_json'
+    path = input_config.get('path')
+    if input_format == 'csv':
+        return pd.read_csv(path)
+
+    # Zeek log in Json format
+    data = []
+    with open(path) as input_file:
+        line = input_file.readline()
+        while line:
+            packet = json.loads(line)
+            data.append(packet)
+            line = input_file.readline()
+    return pd.DataFrame(data)
+
+
+def parse_to_csv(config_file):
+    """
+    Parse an input file to csv file according to a configuration file.
+    :param config_file: Path to the configuration file
+    """
+    config = Config.load_from_file(config_file)
+    input_config = config['input_file']
+    output_file = config.get('output_file', './result.csv')  # Default: './result.csv'
+
+    fields_configs = config['fields']
+    fields = get_fields(fields_configs)
+
+    df = to_dataframe(input_config)
+    result = pd.DataFrame({})
+    parse_field(fields, df, result)
+
+    result.to_csv(output_file, index=False)
+
+
+if __name__ == '__main__':
+    parse_to_csv('./modbus_config.json')
---
+++ parse_func.py
@@ -0,0 +1,104 @@
+import socket
+import sys
+import pandas as pd
+
+current_module = sys.modules[__name__]
+
+
+def parse(value, field, if_handle_abnormal, func_name=None):
+    """
+    Entrance of all the parse functions.
+    :param value: The value of the field.
+    :param field: The field configuration.
+    :param if_handle_abnormal: If the field need abnormal handling.
+    :param func_name: The name of the parse function, or None if no need to parse.
+    :return: The value after abnormal handling and parsing.
+    """
+    # Handle abnormal
+    if if_handle_abnormal:
+        old = value
+        new = handle_abnormal(value, field)
+        if old != new:
+            return new
+
+    # Apply parsing function
+    if func_name is not None:
+        return getattr(current_module, func_name, None)(value)
+    else:
+        return value
+
+
+def second2micro(second):
+    """
+    :param second: Time in second
+    :return: Time in microsecond
+    """
+    return second * 1000000
+
+
+def ip_quad2int(ip_quad_string):
+    """
+    Parse a dotted-quad string IP address (e.g., 192.168.0.0) to a unsigned int
+    by first convert the IP address back to 32-bit binary format and then covert the binary to decimal.
+
+    :param ip_quad_string: IP address in dotted-quad string format (e.g., 192.168.0.0)
+    :return: Converted unsigned int result
+    """
+    # Bytes format of the IP address (192.168.0.0 -> b'\xc0\xa8\x00\x00)
+    ip_bytes = socket.inet_aton(ip_quad_string)
+    # Convert the bytes to a decimal with big endian byte order
+    return int.from_bytes(ip_bytes, 'big')
+
+
+def modbus_func2code(func_name):
+    """
+    :param func_name: Modbus function name
+    :return: Corresponding modbus function code
+    """
+    if func_name == 'READ_COILS':
+        return 1
+    elif func_name == 'READ_DISCRETE_INPUTS':
+        return 2
+    elif func_name == 'READ_HOLDING_REGISTERS':
+        return 3
+    elif func_name == 'READ_INPUT_REGISTERS':
+        return 4
+    elif func_name == 'WRITE_SINGLE_COIL':
+        return 5
+    elif func_name == 'WRITE_SINGLE_REGISTER':
+        return 6
+    else:
+        return 0
+
+
+def handle_abnormal(value, field):
+    """
+    Abnormal handling function.
+    Definition of abnormal:
+        * Value of numbers less than 0;
+        * Empty field.
+    If detected abnormal, will return `unavailable` or `0` according to the desired output format
+    in field configuration;
+    If not, return the original value.
+    :param value: The value to be handled.
+    :param field: The field configuration.
+    :return: The value after abnormal handling.
+    """
+    if field.get('format', 'str') == 'str':
+        abnormal = 'unavailable'
+    else:
+        abnormal = 0
+    if value is None:
+        return abnormal
+    if isinstance(value, str):
+        value = value.strip()
+        if value == '':
+            return abnormal
+        else:
+            return value
+    if isinstance(value, (int, float)):
+        if value < 0 or pd.isna(value):
+            return abnormal
+        else:
+            return value
+    return value
